5b9ec72d879a54b607b99a9f5f0b8e8c6ca313a0
StefiKocheva <stefani291423@gmail.com>
2021-09-13 23:22:56 +0300

    Add project files.

---
+++ TaxiBook/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TaxiBook.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
