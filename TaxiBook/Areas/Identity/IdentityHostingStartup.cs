dae7215ea2c2ff8b1762da2505e57c093b07ff9f
StefiKocheva <stefani291423@gmail.com>
2021-10-21 11:22:35 +0300

    Add Identity

---
+++ TaxiBook/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TaxiBook.Data;
+using TaxiBook.Data.Models;
+
+[assembly: HostingStartup(typeof(TaxiBook.Areas.Identity.IdentityHostingStartup))]
+namespace TaxiBook.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
