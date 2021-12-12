7d83f7c16f687497ebb01564e607a27ae1a32486
Stefani Kocheva <stefani291423@gmail.com>
2021-12-12 14:46:58 +0200

    Edit files

--- Documents/Front-End/Voler template/assets/js/change-profile-image.js
+++ Documents/Front-End/Voler template/assets/js/change-profile-image.js
@@ -1,15 +1,13 @@
-
 $("#profileImage").click(function(e) {
     $("#imageUpload").click();
-  });
+});
   
-  function fasterPreview( uploader ) {
-    if ( uploader.files && uploader.files[0] ){
-          $('#profileImage').attr('src', 
-             window.URL.createObjectURL(uploader.files[0]) );
-    }
-  }
+function fasterPreview( uploader ) {
+  if ( uploader.files && uploader.files[0] ){
+        $('#profileImage').attr('src', window.URL.createObjectURL(uploader.files[0]));
+  };
+};
   
-  $("#imageUpload").change(function(){
-    fasterPreview( this );
-  });
+$("#imageUpload").change(function(){
+  fasterPreview( this );
+});
--- Documents/Front-End/Voler template/assets/js/save-as-fav.js
+++
@@ -1,6 +0,0 @@
-(function() {
-    const heart = document.getElementById('bookmark');
-    heart.addEventListener('click', function() {
-      heart.classList.toggle('change');
-    });
-  })();
