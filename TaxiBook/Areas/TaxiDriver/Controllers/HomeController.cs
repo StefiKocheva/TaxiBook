7138bf00bd91e48ba476afff0461dfd736e07134
Stefani Kocheva <stefani291423@gmail.com>
2021-12-19 21:05:40 +0200

    Add Taxi driver area.

---
+++ TaxiBook/Areas/TaxiDriver/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿namespace TaxiBook.Areas.TaxiDriver.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Threading.Tasks;
+
+    [Authorize]
+    [Area("TaxiDriver")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Schedule()
+        {
+            return View();
+        }
+    }
+}
