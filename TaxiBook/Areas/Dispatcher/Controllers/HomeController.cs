9f81aef00bfed10d65d673e49eebda50791a8e22
Stefani Kocheva <stefani291423@gmail.com>
2021-12-19 21:05:12 +0200

    Add Dispatcher area.

---
+++ TaxiBook/Areas/Dispatcher/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿namespace TaxiBook.Areas.Dispatcher.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Threading.Tasks;
+
+    [Authorize]
+    [Area("Dispatcher")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Schedule()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Track()
+        {
+            return View();
+        }
+    }
+}
---
+++ TaxiBook/Areas/Dispatcher/Controllers/OrdersController.cs
@@ -0,0 +1,41 @@
+﻿namespace TaxiBook.Areas.Dispatcher.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Threading.Tasks;
+
+    [Authorize]
+    [Area("Dispatcher")]
+    public class OrdersController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AcceptedOrders()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> RefusedOrders()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit()
+        {
+            return View();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(string id)
+        {
+            return this.Ok();
+        }
+    }
+}
