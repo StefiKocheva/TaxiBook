94c36f57caa523a3d53a5a53cbf9c3bae8bca304
Stefani Kocheva <stefani291423@gmail.com>
2021-12-30 18:57:23 +0200

    Add migrations.

---
+++ TaxiBook/Data/Migrations/20211228173924_changeCompanyRequirments.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TaxiBook.Data.Migrations
+{
+    public partial class changeCompanyRequirments : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PricePerKilometer",
+                table: "Taxies");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Companies",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Companies",
+                type: "nvarchar(300)",
+                maxLength: 300,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "PricePerKilometer",
+                table: "Companies",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Companies");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Companies");
+
+            migrationBuilder.DropColumn(
+                name: "PricePerKilometer",
+                table: "Companies");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "PricePerKilometer",
+                table: "Taxies",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
