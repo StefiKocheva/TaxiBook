c0d6121dc9ff1166554abb79e95b584ec95ca9cf
Stefani Kocheva <stefani291423@gmail.com>
2021-12-30 18:56:59 +0200

    Create new view models.

---
+++ TaxiBook/Areas/TaxiDriver/ViewModels/Home/ForthcomingАbsenceViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TaxiBook.Areas.TaxiDriver.ViewModels.Home
+{
+    using System;
+
+    public class ForthcomingАbsenceViewModel
+    {
+        public DateTime From { get; set; }
+
+        public DateTime Till { get; set; }
+    }
+}
